279e968f130c609226fc56bca7e524eee55ce356
tomo2 <tomo2321@gmail.com>
2026-03-10 02:19:50 +0900

    chore: add starship setting

--- setup.sh
+++ setup.sh
@@ -14,12 +14,16 @@ chsh -s /bin/zsh
 
 DOTFILES_DIR="$(cd $(dirname "$0") && pwd)"
 
+XDG_CONFIG_HOME="$HOME/.config"
+mkdir -p $XDG_CONFIG_HOME
+
 ln -s $DOTFILES_DIR/.zprofile ~/.zprofile
 ln -s $DOTFILES_DIR/.zshrc ~/.zshrc
 ln -s $DOTFILES_DIR/vimrc ~/.vimrc
 ln -s $DOTFILES_DIR/vim ~/.vim
 ln -s $DOTFILES_DIR/git/.gitconfig ~/.gitconfig
 ln -s $DOTFILES_DIR/git/.gitignore ~/.gitignore
+ln -s $DOTFILES_DIR/starship/starship.toml $XDG_CONFIG_HOME/starship.toml
 if [ "$(which -a nvim)" ]; then
   mkdir -p ~/.config/nvim
   ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
