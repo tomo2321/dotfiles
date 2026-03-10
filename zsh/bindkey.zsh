866049a6ef0063553f38aaced28c92e9945792a9
tomo2 <30072176+tomo2321@users.noreply.github.com>
2026-03-11 00:28:13 +0900

    chore: add Ctrl-g bindkey setting

--- zsh/bindkey.zsh
+++ zsh/bindkey.zsh
@@ -2,3 +2,16 @@
 autoload -Uz edit-command-line
 zle -N edit-command-line
 bindkey '^x^e' edit-command-line
+
+# Bind Ctrl-g to search and navigate to a ghq-managed repository using fzf
+# https://zenn.dev/mozumasu/articles/mozumasu-lazy-git
+function ghq-fzf() {
+  local src=$(ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*")
+  if [ -n "$src" ]; then
+    BUFFER="cd $(ghq root)/$src"
+    zle accept-line
+  fi
+  zle -R -c
+}
+zle -N ghq-fzf
+bindkey '^g' ghq-fzf
