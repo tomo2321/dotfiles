9bed7aad4e12fc15d94159af121433fe4a2a3a3f
tomo2 <30072176+tomo2321@users.noreply.github.com>
2026-03-29 20:08:09 +0900

    feat: add dump brewfile function

--- zsh/function.zsh
+++ zsh/function.zsh
@@ -5,3 +5,8 @@ function decrypt-pdf() {
   fi
   qpdf --decrypt "$1" "$2"
 }
+
+function dump-brewfile() {
+  DOTFILES_DIR="${${(%):-%N}:A:h}"
+  brew bundle dump --file="$DOTFILES_DIR/homebrew/Brewfile" --force
+}
